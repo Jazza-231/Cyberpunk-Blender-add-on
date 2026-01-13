aa349f628e7c1b69eaa20e4fa406b2c83e5c92f6
Jazza231 <greeny-231@outlook.com>
2026-01-14 00:57:11 +0800

    Fixes

--- i_scene_cp77_gltf/material_types/particles_hologram.py
+++ i_scene_cp77_gltf/material_types/particles_hologram.py
@@ -19,8 +19,13 @@ class ParticlesHologram:
         texture_coords = CurMat.nodes.new("ShaderNodeTexCoord")
         vec_mul = CurMat.nodes.new("ShaderNodeVectorMath")
         vec_mul.operation = "MULTIPLY"
+        scale_vec_mul = CurMat.nodes.new("ShaderNodeVectorMath")
+        scale_vec_mul.operation = "MULTIPLY"
 
         aspect = CurMat.nodes.new("ShaderNodeValue")
+        scale = CurMat.nodes.new("ShaderNodeValue")
+        scale_aspect = CurMat.nodes.new("ShaderNodeMath")
+        scale_aspect.operation = "MULTIPLY"
         combine_xyz = CurMat.nodes.new("ShaderNodeCombineXYZ")
 
         vec_map = CurMat.nodes.new("ShaderNodeMapping")
@@ -55,13 +60,14 @@ class ParticlesHologram:
         # Populate nodes
 
         aspect.outputs[0].default_value = 1.32802  # Kinda arbitrary, what I measured
+        scale.outputs[0].default_value = 9.0
 
         combine_xyz.inputs[0].default_value = 1
         combine_xyz.inputs[1].default_value = 1
         combine_xyz.inputs[2].default_value = 1
 
-        vec_mul.inputs[1].default_value = (13.0, 13.0, 13.0)
-        dots_mul.inputs[1].default_value = 30.0
+        vec_mul.inputs[1].default_value = (13.0, 1.5, 1.5)
+        dots_mul.inputs[1].default_value = 5.0
         dots_mul.use_clamp = True
 
         emission_mul.inputs[0].default_value = 1.0
@@ -88,10 +94,16 @@ class ParticlesHologram:
         CurMat.links.new(texture_coords.outputs[2], vec_mul.inputs[0])
         CurMat.links.new(texture_coords.outputs[2], vec_map.inputs[0])
 
-        CurMat.links.new(aspect.outputs[0], combine_xyz.inputs[0])
+        CurMat.links.new(aspect.outputs[0], scale_aspect.inputs[0])
+        CurMat.links.new(scale.outputs[0], scale_aspect.inputs[1])
+
+        CurMat.links.new(vec_mul.outputs[0], scale_vec_mul.inputs[0])
+        CurMat.links.new(scale.outputs[0], scale_vec_mul.inputs[1])
+
+        CurMat.links.new(scale_aspect.outputs[0], combine_xyz.inputs[0])
         CurMat.links.new(combine_xyz.outputs[0], vec_map.inputs[3])
 
-        CurMat.links.new(vec_mul.outputs[0], dots.inputs[0])
+        CurMat.links.new(scale_vec_mul.outputs[0], dots.inputs[0])
         CurMat.links.new(dots.outputs[0], dots_mul.inputs[0])
 
         CurMat.links.new(vec_map.outputs[0], alpha_mask.inputs[0])
